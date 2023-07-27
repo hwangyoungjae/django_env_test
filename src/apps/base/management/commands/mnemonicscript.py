da79f60aefc5f7d700906f7218522d6086af053b
hwangyoungjae <dudwo56@gmail.com>
2023-07-27 18:33:51 +0900

    -

--- src/apps/base/management/commands/mnemonicscript.py
+++ src/apps/base/management/commands/mnemonicscript.py
@@ -1,23 +1,11 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
-from datetime import date
-from typing import Any, Literal
+from typing import Any
 
 from django.core.management import BaseCommand
 from web3 import Web3
 
 from inject_property import InjectProperty
-
-
-@dataclass
-class Entry:
-    case: Literal['go', 'leave']
-    date: date
-    distance: int
-    running_minute: int
-    idling_minute: int
-    avg_speed: int
 
 
 class Command(BaseCommand):
